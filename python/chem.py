766c461cca5aff1b5e82ee7d571463f041b8e583
Vitaliy Batusov <vbatusov@scs.ryerson.ca>
2014-06-02 20:38:48 -0400

    Initial version of the Python app that parses RXN and renders it using Indigo toolkit

---
+++ python/chem.py
@@ -0,0 +1,363 @@
+import copy
+import random
+
+
+class Atom:
+    'This class is geared to store atom information supplied by v3000 molfiles'
+
+    def __init__(self, symbol, x, y, z, rxnIndex, rxnAAM, attribs={}):
+        self.symbol = symbol
+        self.x = x
+        self.y = y
+        self.z = z
+        self.rxnIndex = rxnIndex
+        self.rxnAAM = rxnAAM
+        self.attribs = attribs
+        self.mass = 0
+        self.charge = 0
+
+    def __str__(self):
+        return "<Atom " + self.symbol + " at (" + ", ".join([str(self.x), str(self.y), str(self.z)]) + ") with AAM=" + str(self.rxnAAM) + " and attribs " + str(self.attribs) + " />"
+
+class Bond:
+    'v3000 bond information container'
+
+    'fromAtom and toAtom must be Atom objects!'
+    def __init__(self, rxnIndex, order, fromAtom, toAtom, attribs={}):
+        self.rxnIndex = rxnIndex
+        self.order = order
+        self.fromAtom = fromAtom
+        self.toAtom = toAtom
+        self.attribs = attribs
+
+    def __str__(self):
+        return "<Bond with order " + str(self.order) + " between AAM " + str(self.fromAtom.rxnAAM) + " and " + str(self.toAtom.rxnAAM) + " />"
+
+class Molecule:
+    """ A molecule can be either a complete molecule or a fragment.
+    The difference is in the self.anchor: it is the attachment point
+    which for a fragment is a pointer to one of its atoms and for a
+    complete molecule is None.
+
+    A molecule may contain pseudoatoms ("Alkyl") and R-group atoms ("R1").
+    """
+
+    def __init__(self):
+        self.atomList = []
+        self.bondList = []
+        self.anchor = None
+
+    def addAtom(self, atom):
+        self.atomList.append(atom)
+
+    def addBond(self, bond):
+        self.bondList.append(bond)
+
+    def __str__(self):
+        desc = "<Molecule>"
+        for atom in self.atomList:
+            desc += "\n  " + str(atom)
+        for bond in self.bondList:
+            desc += "\n  " + str(bond)
+        desc += "\n</Molecule>"
+        return desc
+
+    @property
+    def numberOfAtoms(self):
+        return len(self.atomList)
+
+    def getIndigoObject(self, indigo):
+        #print "CALLING getIndigoObject with indigo=" + str(indigo)
+        indigo_mol = indigo.createMolecule()
+        indigo_atoms = {}
+        for atom in self.atomList:
+            indigo_atoms[atom.rxnAAM] = indigo_mol.addAtom(atom.symbol)
+            print "Added indigo atom " + indigo_atoms[atom.rxnAAM].symbol()
+        for bond in self.bondList:
+            indigo_atoms[bond.fromAtom.rxnAAM].addBond(indigo_atoms[bond.toAtom.rxnAAM], bond.order)
+
+        return indigo_mol
+
+    def replaceAtomWithMolecule(self, oldatom, newmolecule):
+        """ Used by Molecule.getInstance, this method alters 'self' by replacing a given atom
+        object with another one in atomList, and then correcting the bonds to point to the
+        new atom. Finally, the remaining atoms and bonds in given molecule are added to 'self'.
+        """
+
+        if newmolecule.anchor is None:
+            raise Exception("New molecule does not have an anchor!")
+
+        newmolecule.anchor.rxnAAM = oldatom.rxnAAM
+
+        # Correct the bonds to anchor of new molecule
+        for bond in self.bondList:
+            if bond.toAtom is oldatom:
+                bond.toAtom = newmolecule.anchor
+            if bond.fromAtom is oldatom:
+                bond.fromAtom = newmolecule.anchor
+
+        # Add remaining atoms and bonds to current molecule
+        self.atomList = filter(lambda a: a is not oldatom, self.atomList)
+        self.atomList.extend(newmolecule.atomList)  # Is extend shallow or deep? Assume shallow.
+        self.bondList.extend(newmolecule.bondList)
+
+
+        # FINISHED, UNTESTED
+
+
+    def getInstance(self, rgroups={}):
+        """ This returns a new molecule which is the same as self except that every
+        R-atom is replaced with a corresponding molecule from the provided dictionary.
+        """
+        # This will be returned
+        newmol = copy.deepcopy(self)
+
+        #print "Molecule instantiation in progress..."
+        #print "New copy of original:\n" + str(newmol)
+
+        # In the copy, replace each occurence of an R-atom with an actual R-instance
+        for atom in newmol.atomList:
+            #print "Looking at atom " + atom.symbol
+            if atom.symbol in rgroups.keys():
+                print "An R-group is found! Replacing it with a sub-molecule..."
+                newmol.replaceAtomWithMolecule(atom, rgroups[atom.symbol])
+            
+        return newmol
+
+        # FINISHED, UNTESTED
+
+
+
+
+class Reaction:
+    """stores a set of reactants, agents, products, and rgroups
+
+    rgroups is a dictionary "R1" : [R-molecule, R-molecule, ...]
+    """
+
+    def __init__(self):
+        self.reactants = []
+        self.agents = []
+        self.products = []
+        self.rgroups = {}
+
+    def __str__(self):
+        desc = "<Reaction>"
+        desc += "\n  <Reactants>"
+        for r in self.reactants:
+            desc += "\n    " + ('\n    ').join(str(r).split('\n'))
+        desc += "\n  </Reactants>\n  <Agents>"
+        for a in self.agents:
+            desc += "\n    " + ('\n    ').join(str(a).split('\n'))
+        desc += "\n  </Agents>\n  <Products>"
+        for p in self.products:
+            desc += "\n    " + ('\n    ').join(str(p).split('\n'))
+        desc += "\n  </Products>\n  <R-groups>"
+        for g in self.rgroups.keys():
+            desc += "\n    <R-group No. " + g + ">"
+            for g2 in self.rgroups[g]:
+                desc += "\n      " + ('\n      ').join(str(g2).split('\n'))
+            desc += "\n    </R-group No. " + g + ">"
+        desc += "\n  </R-groups>\n<Reaction>"
+
+        return desc
+
+    @property
+    def numberOfAtoms(self):
+        """ Counts all atoms (including pseudo and R) in reaction.
+        """
+        num = 0
+        
+        # Assume agents don't participate and are irrelevant
+
+        for mol in self.reactants:
+            num += mol.numberOfAtoms
+
+        return num
+
+    def addReactant(self, molecule):
+        self.reactants.append(molecule)
+
+    def addAgent(self, molecule):
+        self.agents.append(molecule)
+
+    def addProduct(self, molecule):
+        self.products.append(molecule)
+
+    def addRGroup(self, rgroupNumber, molecule):
+        rgroupName = "R" + str(rgroupNumber)
+        if rgroupName not in self.rgroups:
+            self.rgroups[rgroupName] = []
+        self.rgroups[rgroupName].append(molecule)
+
+    def selectRGroups(self):
+        """ For each unique R-group in reaction, select a single generic
+        fragment out of the set of possible ones. Do not unroll the pseudoatoms.
+        """
+
+        # This will be returned
+        fragments = {}
+
+        # For each R-group name
+        for rgroupName in self.rgroups.keys():
+            # Randomly pick one fragment (one R-group may have multiple)
+            fragments[rgroupName] = copy.deepcopy(random.choice(self.rgroups[rgroupName]))
+
+        return fragments
+
+    def getInstance(self):
+        """ Returns an instantiated (non-generic) reaction.
+        Logic:
+            1. For each r-group in reaction, select a generic instance and
+                substitute it for the respective r-group atom on both sides
+                of the reaction.
+            2. For each unique pseudoatom in reaction, generate a non-generic
+                instance and substitute it for the respective atom on both
+                sides of reaction.
+            The result should be an entirely new reaction object.
+        """
+
+        # DEBUG
+        print "Building a reaction instance..."
+
+        # This will be returned 
+        reactionInst = Reaction()
+
+        # Select specific generic fragments to be used as R-groups
+        fragmentsRG = self.selectRGroups()
+
+        # Substitute selected R-fragments into every reactant, agent, and product
+        for molecule in self.reactants:
+            reactionInst.addReactant(molecule.getInstance(fragmentsRG))
+
+        for molecule in self.agents:
+            reactionInst.addAgent(molecule.getInstance(fragmentsRG))
+
+        for molecule in self.products:
+            reactionInst.addProduct(molecule.getInstance(fragmentsRG))
+
+        print "BEFORE ENUMERATION++++++++++++++"
+        print str(reactionInst)
+
+        # To finish with R-groups, assign AAM numbers where necessary
+        # Note: it is enough to set AAM in reactants only, since they are
+        # stored as references and will change across the entire reaction.
+        nextAAM = reactionInst.numberOfAtoms
+        for molecule in reactionInst.reactants:
+            for atom in molecule.atomList:
+                if atom.rxnAAM == 0:
+                    atom.rxnAAM = nextAAM
+                    nextAAM -= 1
+
+        # Now, unwrap the pseudoatoms.
+        # In each reactant, for each pseudoatom, generate a fragment
+        # and substitute with it every occurence of the pseudoatom in the products.
+        fragmentsPseudo = {}
+        for molecule in reactionInst.reactants:
+            for atom in molecule.atomList:
+                if atom.symbol in PSEUDO.keys():
+                    print "A pseudoatom is found!"
+                    fragmentsPseudo[str(atom.rxnAAM)] = getInstanceByName(atom)
+
+        # Iterate through fragments and assign aam to all atoms
+        nextAAM = reactionInst.numberOfAtoms + 1
+        for frag in fragmentsPseudo.values():
+            for atom in frag.atomList:
+                if atom.rxnAAM == 0:
+                    atom.rxnAAM = nextAAM
+                    nextAAM += 1
+
+        # Iterate through both reactants and products,
+        # replacing corresponding aam atom with fragment
+        for molecule in reactionInst.reactants:
+            for atom in molecule.atomList:
+                if str(atom.rxnAAM) in fragmentsPseudo.keys():
+                    molecule.replaceAtomWithMolecule(atom, fragmentsPseudo[str(atom.rxnAAM)])
+
+        for molecule in reactionInst.products:
+            for atom in molecule.atomList:
+                if str(atom.rxnAAM) in fragmentsPseudo.keys():
+                    molecule.replaceAtomWithMolecule(atom, fragmentsPseudo[str(atom.rxnAAM)])
+
+        return reactionInst
+
+
+
+
+def getInstanceByName(atom): # TODO: fix signature everywhere
+    """ 
+    """
+    return PSEUDO[atom.symbol](atom.rxnAAM)
+
+
+def buildAlkyl(anchorAAM, size=2):
+    """ Must return a molecule whose anchor is not None """
+
+    if size < 1:
+        raise Exception("Alkyl size cannot be less than one!")
+
+    molecule = Molecule()
+    root = Atom("C", 0, 0, 0, 0, 0)
+    root.rxnAAM = anchorAAM
+    molecule.addAtom(root)
+    molecule.anchor = root
+    
+    (size1, size2, size3) = splitThreeWays(size - 1)
+    
+    buildAlkylTree(molecule, root, size1)
+    buildAlkylTree(molecule, root, size2)
+    buildAlkylTree(molecule, root, size3)
+
+    return molecule
+
+def buildAlkylTree(molecule, anchor, size) :
+    if size < 1 :
+        atom = Atom("H", 0, 0, 0, 0, 0)
+
+        molecule.addAtom(atom)
+        if anchor is not None:
+            molecule.addBond(Bond(0, 1, anchor, atom))
+    else : # assume size is non-negative
+        # create new carbon
+        atom = Atom("C", 0, 0, 0, 0, 0)
+
+        molecule.addAtom(atom)
+        # link it back to parent
+        if anchor is not None:
+            molecule.addBond(Bond(0, 1, anchor, atom))
+
+        (size1, size2, size3) = splitThreeWays(size - 1)
+        
+        buildAlkylTree(molecule, atom, size1)
+        buildAlkylTree(molecule, atom, size2)
+        buildAlkylTree(molecule, atom, size3)
+
+
+def splitThreeWays(number):
+    a = random.random()
+    b = random.random()
+    c = random.random()
+    whole = a + b + c
+    coeff = number / whole
+    resA = int(round(coeff * a))
+    resB = int(round(coeff * b))
+    resC = int(number - resA - resB)
+    #print "Splitting " + str(number) + " yields " + str((resA,resB,resC))
+    return (resA,resB,resC)
+
+
+
+def buildHalogen():
+    return None
+
+PSEUDO = {"Alkyl" : buildAlkyl, "Halogen" : buildHalogen}
+
+
+
+
+
+
+
+
+
+# Setting AAM to new atoms is very messy.
---
+++ python/parse_rxn.py
@@ -0,0 +1,202 @@
+import textfsm
+import re
+import chem
+import sys
+sys.path.append('/home/vitaliy/chemistry/indigo-python-1.1.12/')
+from indigo import *
+from indigo_renderer import *
+import base64
+import os
+
+(ACTOR_TYPE,
+ACTOR_ARG,
+RLOGIC,
+ACTOR_CHIRAL,
+ATOM_INDEX,
+ATOM_TYPE,
+ATOM_X,
+ATOM_Y,
+ATOM_Z,
+ATOM_AAM,
+ATOM_ATTR,
+BOND_INDEX,
+BOND_TYPE,
+BOND_FROM,
+BOND_TO,
+BOND_ATTR) = range(16)
+
+
+def parse_attribs(attribs):
+    """ Parse the silly RXNv3000 atom/bond parameter string 
+    and store each key-value pair as a dictionary. """
+
+    attribDict = {}
+
+    # Matches things like "RGROUPS=(1 1) MASS=16",
+    # only the 2nd and 3rd groups in each match are important
+    match = re.findall('((\S+)=((\(.*?\))|([^(]\S*)))+', attribs)
+
+    for (_,name,val,_,_) in match:
+        # If value is a RXN list, turn it into a proper list
+        matchVal = re.match('^\((.*)\)$',val)
+        if matchVal:
+            # Split the relevant group on space and discard the first (count) item
+            attribDict[name] = matchVal.group(1).split()[1:]
+        else:
+            attribDict[name] = val
+
+    return attribDict
+
+def parse_record(record, rgroup=False):
+    """ A record is a list of strings and lists of strings.
+    Each record defines a single (possibly generic) molecule. However,
+    a definition of a single R-group may consist of multiple molecules.
+    Thus, an R-group should be a list of molecules, and so parse_record
+    should return not simply a molecule, but also a note about
+    which R-group the molecule belongs to."""
+    
+    # This will be returned
+    molecule = chem.Molecule()
+
+    # A map from rxn index to the corresponding atom object,
+    # necessary for creating bonds later
+    indexToAtom = {}
+
+    # Extract the defining properties of each atom from the record using pre-defined position indeces
+    for (index, symbol, x, y, z, aam, attr) in zip(record[ATOM_INDEX], record[ATOM_TYPE], record[ATOM_X], record[ATOM_Y], record[ATOM_Z], record[ATOM_AAM], record[ATOM_ATTR]):
+        
+        # Convert the attribute string to a dictionary
+        attrDict = parse_attribs(attr)
+
+        # Correct the symbol if it is an R-group
+        if symbol == "R#":
+            symbol = "R" + attrDict["RGROUPS"][0]
+        
+        # Create an atom object
+        atom = chem.Atom(symbol, float(x), float(y), float(z), int(index), int(aam), attrDict)
+        indexToAtom[index] = atom
+
+        # Add atom to molecule
+        molecule.addAtom(atom)
+
+        if rgroup and attrDict["ATTCHPT"] == "1":
+            molecule.anchor = atom
+
+
+    # Extract the defining properties of each bond
+    for (index, order, fromAtom, toAtom, attr) in zip(record[BOND_INDEX], record[BOND_TYPE], record[BOND_FROM], record[BOND_TO], record[BOND_ATTR]):
+
+        attrDict = parse_attribs(attr)
+
+        # Create a bond object: to- and from-atom arguments are actual objects;
+        # their RXN indeces are preserved in the objects themselves
+        bond = chem.Bond(int(index), int(order), indexToAtom[fromAtom], indexToAtom[toAtom], attrDict)
+
+        # Add bond to molecule
+        molecule.addBond(bond)
+
+    #print str(molecule) + "\n"
+    
+
+    # DEBUG render the molecule; this tests both structure and indigo interaction
+    indigo_molecule = molecule.getIndigoObject(indigo)
+    smiles = indigo_molecule.canonicalSmiles()
+    #print "Created new " + str(molecule) + " with SMILES " + smiles
+    safename = base64.urlsafe_b64encode(smiles)
+    indigo_molecule.layout()
+    renderer.renderToFile(indigo_molecule, "img/raw-" + safename + ".png");
+    print " -> Written the molecule image to " + safename + ".png"
+
+    return molecule
+
+def parse_rxn(rxn, fsm="rxn.fsm"):
+    """ Parses the silly RXNv3000 and creates a generic reaction as a result.
+    Hack: (TODO) when reading raw data, merge lines on dash. This will elegantly (in some sense)
+    handle the silly RXNv3000 80-char ugliness without making a mess out of the FSM.
+    """
+
+    # Load the regex
+    re_table = textfsm.TextFSM(open(fsm))
+
+    # Load the raw RXN
+    text = ''.join(open(rxn).readlines())
+
+    # Parse the RXN using the state machine
+    # Data is a list of TextFSM records
+    data = re_table.ParseText(text)
+
+    # This will be returned
+    reaction = chem.Reaction()
+
+    # Reminder: each record stores a complete description of a single molecule
+    for record in data:
+        print record
+        if record[ACTOR_TYPE]=="REACTANT":
+            #print "Found reactant"
+            reaction.addReactant(parse_record(record))
+
+        elif record[ACTOR_TYPE]=="PRODUCT":
+            #print "Found product"
+            reaction.addProduct(parse_record(record))
+
+        elif record[ACTOR_TYPE]=="AGENT":
+            #print "Found agent"
+            reaction.addAgent(parse_record(record))
+
+        elif record[ACTOR_TYPE]=="RGROUP":
+            #print "Found an R-group"
+            reaction.addRGroup(record[ACTOR_ARG], parse_record(record, True))
+
+        else:
+            raise Exception("Unknown record type!")
+
+    return reaction
+
+
+print "\n\n************   S T A R T   A G A I N   **************\n"
+
+folder = 'img'
+for the_file in os.listdir(folder):
+    file_path = os.path.join(folder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    except Exception, e:
+        print e
+
+indigo = Indigo()
+renderer = IndigoRenderer(indigo)
+outputFormat = "png"
+indigo.setOption("render-output-format", outputFormat)
+indigo.setOption("render-margins", 10, 10)
+indigo.setOption("render-coloring", True)
+indigo.setOption("render-bond-length",50)
+indigo.setOption("render-atom-ids-visible", False)
+indigo.setOption("render-aam-color", 0.5, 0.5, 1.0)
+
+reaction = parse_rxn("amide.rxn")
+print "\n" + str(reaction)
+
+print "\n.........   Instance   ............"
+
+reactionInst = reaction.getInstance()
+print "\n" + str(reactionInst)
+
+for reactant in reactionInst.reactants:
+    indo = reactant.getIndigoObject(indigo)
+    smiles = indo.canonicalSmiles()
+    #print "Created new " + str(molecule) + " with SMILES " + smiles
+    safename = base64.urlsafe_b64encode(smiles)
+    indo.layout()
+    renderer.renderToFile(indo, "img/reactant-" + safename + ".png");
+    print " -> Written the molecule image to " + safename + ".png"
+
+for product in reactionInst.products:
+    indo = product.getIndigoObject(indigo)
+    smiles = indo.canonicalSmiles()
+    #print "Created new " + str(molecule) + " with SMILES " + smiles
+    safename = base64.urlsafe_b64encode(smiles)
+    indo.layout()
+    renderer.renderToFile(indo, "img/product-" + safename + ".png");
+    print " -> Written the molecule image to " + safename + ".png"
+
