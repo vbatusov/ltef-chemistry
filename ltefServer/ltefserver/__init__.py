c1835f275766da815682c5e4d0b1b57b0550ed3f
Vitaliy Batusov <vbatusov@lto.scs.ryerson.ca>
2014-06-24 14:50:49 -0400

    Rearranged directory structure

---
+++ ltefServer/ltefserver/__init__.py
@@ -0,0 +1,24 @@
+from pyramid.config import Configurator
+
+#from pyramid.httpexceptions import HTTPUnauthorized
+from pyramid.response import Response
+
+#def myviewfunction(request):
+#    return HTTPUnauthorized()
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+    config.include('pyramid_chameleon')
+    config.add_static_view('static', 'static', cache_max_age=3600)
+
+    config.add_route('home', '/')
+    config.add_route('tools', '/tools')
+    config.add_route('synthesis', '/tools/synthesis')
+    config.add_route('learning', '/tools/learning')
+    config.add_route('learning_reaction', '/tools/learning/{basename}')
+    config.add_route('pic_generic', '/img/generic_{basename}.png')
+    config.add_route('pic_instance', '/img/instance_{basename}.png')
+    config.scan()
+    return config.make_wsgi_app()
