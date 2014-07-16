a30a342244e7c268484c3d3336d5b172ef0ba53e
Vitaliy Batusov <vbatusov@scs.ryerson.ca>
2014-07-16 17:07:00 -0400

    Added About and Contact. Contact writes messages to a local file, does not send any emails.

--- ltefServer/ltefserver/__init__.py
+++ ltefServer/ltefserver/__init__.py
@@ -34,6 +34,8 @@ def main(global_config, **settings):
     config.add_route('home', '/')
     config.add_route('tools', '/tools')
     config.add_route('synthesis', '/tools/synthesis')
+    config.add_route('about', '/about')
+    config.add_route('contact', '/contact')
     config.add_route('learning', '/tools/learning')
     config.add_route('learning_reaction', '/tools/learning/{basename}')
     config.add_route('img', '/img/{basename}/{what}/{filename}.png')
