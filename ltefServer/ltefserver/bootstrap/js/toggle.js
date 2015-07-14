e25bea924cfb2f8c8f2c2406fcaee23902e89dba
DanielK <daniel.kozij@gmail.com>
2015-07-13 19:21:53 -0500

    Cleaned old style code that wasn't being used

--- ltefServer/ltefserver/bootstrap/js/toggle.js
+++ ltefServer/ltefserver/bootstrap/js/toggle.js
@@ -1,5 +1,6 @@
-
-
+/*
+Button Toggle hides and shows the side navigation 
+*/
 $(document).ready(function(){
     $("button.toggle").click(function(){
         $("#wrapper").toggleClass("toggled");
