a9f045666d6886b8cb59ad45b2ac5b368d686d1a
DanielKozij <daniel.kozij@gmail.com>
2015-09-28 11:31:47 -0500

    Prodoct Quizzes have been updated and completed

--- ltefServer/ltefserver/views/quiz.py
+++ ltefServer/ltefserver/views/quiz.py
@@ -437,7 +437,6 @@ def update_svg_size(svg, width, height):
 
     updated_svg = svgline + "\n" + svg[svg.find('\n') + 1 :]
 
-
     return updated_svg
 
 
@@ -445,50 +444,49 @@ def update_svg_size(svg, width, height):
 @view_config(route_name='quiz', match_param='quiz_type=products', renderer='ltefserver:templates/new/quiz_product_reaction.pt', permission='study')
 def quiz_product_view(request):
 
-    # global variable instance reaction stores the whole reaction question
-    global instance_reaction
-    # globel variable instance choice stores a list of molecule choice questions
-    global instance_choices
-    # globel variable correct_choices stores a list of the correct choice position
-    global correct_choices
-
+    basename = ""
+    page_title = ""
+    message = ""
+    owner_courses = []
+    enrolled_courses = []
     class_name = request.matchdict["course"]
     chapter_name = request.matchdict["chapter"]
     mode = request.matchdict["basename"]
     reaction_type = request.matchdict["quiz_type"]
     currentuser = User.current_user(request.authenticated_userid)
+
+    # Two type of states Ask the question or Tell if answer by result being correct or incorrect
+    state = ""
+    result = False
+
     custom_scripts = []
     custom_scripts.append("/bootstrap/js/quiz_reactants.js")
 
     # Get the users security group
     group = group_security(request.authenticated_userid)
     if group["is_teacher"]:
-         course = DBSession.query(Course).filter(Course.name == class_name, Course.owner == currentuser.id ).first()
+        owner_courses = Course.owner_courses(request.authenticated_userid)
+        course = DBSession.query(Course).filter(Course.name == class_name, Course.owner == currentuser.id ).first()
     elif group["is_student"]:
+        enrolled_courses = Enrolled.enrolled_courses(request.authenticated_userid)
         course = DBSession.query(Course).filter(Enrolled.courseid == Course.id).filter(Enrolled.userid == currentuser.id).filter(Course.name == class_name).first()
+
     # Get the current Chapter being used
     chapter = DBSession.query(Chapter).filter( Chapter.title == chapter_name, Chapter.course == course.id ).first()
 
-    session = request.session
-    basename = ""
-    full_name = ""
-    message = ""
+    # Where to store the generated un answered question ? Will be stored in Gerenated_question database table
+        # Gerenated_question id, user, quiz_type, full_reaction, choices
+    generated_question = DBSession.query(Generated_question).filter(Generated_question.chapter == chapter.id).filter(Generated_question.user == currentuser.id).filter(Generated_question.quiz_type == reaction_type).first()
 
-    # Two type of states Ask the question or Tell if answer by result being correct or incorrect
-    state = "ask"
-    result = False
+    reaction_svg = ""
+    question_svg = ""
+    choices_svg = []
 
-    global question_svg
-    global reaction_svg
-    global product_svgs
+    # if a current_user has no generated question then generate a reaction question
+    if generated_question is None:
 
-    # Generate a problem, store the objects, present to user
-    if 'quiz_type' not in session or session['quiz_type'] != 'products' or question_svg == "" :
-
-        session.invalidate()
-        session['quiz_type'] = 'products'
+        # change state to ask
         state = "ask"
-
 
         # if mode is random select a reaction randomly
         if mode == "random":
@@ -496,41 +494,36 @@ def quiz_product_view(request):
         else:
             basename = mode
 
-        # retrieve the raction
+        # retrieve the reaction
         reaction = cat.get_reaction_by_basename(basename)
-        # Get it's full reaction name
-        full_name = reaction.full_name
+        reac_id = DBSession.query(Reac).filter(Reac.basename == basename ).first().id
 
-        # prepare instance, cut off reactants
-        instance = reaction.getInstance()
-        instance_reaction = copy.deepcopy(instance)
-        reactants = instance.reactants
-        products = instance.products
+        custom_reaction = DBSession.query(Customizable_reaction).filter(Customizable_reaction.chapter == chapter.id ).filter(Customizable_reaction.reaction == reac_id ).first()
+
+        # Get it's full reaction name
+        page_title = custom_reaction.title
+
+        # prepare full reaction instance
+        instance_full_reaction = reaction.getInstance()
+        instance_reaction = copy.deepcopy(instance_full_reaction)
+
+        # prepare reactants and products instances
+        reactants = instance_full_reaction.reactants
+        products = instance_full_reaction.products
 
         # Initialize draw SVGRenderer object
         svg_reanderer = draw.SVGRenderer()
-        # Draw the full image of the reaction
-        reaction_svg = ""
-        reaction_svg = svg_reanderer.renderReactionToBuffer(instance, layout=False)
 
         # Hide the reactant from the Question and add a question mark
         molecule = chem.Molecule()
         molecule.addAtom(chem.Atom("?", 0, 0, 0, 0, 0))
-        instance.products = [molecule]
+        instance_full_reaction.products = [molecule]
 
-        # Reaction image without reactants
+        # render image svg without reactants
         question_svg = ""
-        question_svg = svg_reanderer.renderReactionToBuffer(instance, layout=False)
+        question_svg = svg_reanderer.renderReactionToBuffer(instance_full_reaction, layout=False)
+        question_svg = update_svg_size(question_svg, '100%', '-1')
 
-        # Chop off the xml tag
-        question_svg = question_svg[question_svg.find('\n') + 1:]
-        # Modify height and width of the svg tag
-        svgline = question_svg[:question_svg.find('\n')]
-        svglineparts = re.split('width=".*?"', svgline)
-        svgline = svglineparts[0] + 'width="100%"' + svglineparts[1]
-        question_svg = svgline + "\n" + question_svg[question_svg.find('\n') + 1 :]
-
-        product_svgs = []
         instance_choices = []
 
         # Add all the correct choices to instance_choices
@@ -547,80 +540,113 @@ def quiz_product_view(request):
         # shuffle the molecule choices
         random.shuffle(instance_choices)
 
-        correct_choices = []
+        choices_svg = []
         # Draw the molecule choices and store the correct choices in a global set correct_choices
         for index in range(0,  len(instance_choices)):
-            svg =  svg_reanderer.renderMoleculeToBuffer(instance_choices[index][0], layout=False)
-            # Chop off the xml tag
-            svg = svg[svg.find('\n') + 1:]
-            # Modify height and width of the svg tag
-            svgline = svg[:svg.find('\n')]
-            svglineparts = re.split('height=".*?"', svgline)
-            svgline = svglineparts[0] + 'height="100%" "' + svglineparts[1]
-            svglineparts = re.split('width=".*?"', svgline)
-            svgline = svglineparts[0] + 'width="100%"' + svglineparts[1]
-            svg = svgline + "\n" + svg[svg.find('\n') + 1 :]
+            svg = svg_reanderer.renderMoleculeToBuffer(instance_choices[index][0], layout=False)
+            svg = update_svg_size(svg, '100%', '100%')
+            choices_svg.append([svg, instance_choices[index][1]])    # indicate that these are correct answers
 
-            product_svgs.append([svg, instance_choices[index][1]])    # indicate that these are correct answers
+        DBSession.add(Generated_question( user = currentuser.id, chapter = chapter.id , quiz_type = reaction_type, reaction = reac_id, full_reaction = instance_reaction, choices = instance_choices ))
+    # elif a current_user has answered the question remove the generated question and tell the result
+
+    elif generated_question is not None and "answer" in request.GET:
+
+        # change state to tell user the result
+        state = 'tell'
+
+        # get the anser provided by the user
+        answer = request.GET["answer"].split(",")
+
+        custom_reaction = DBSession.query(Customizable_reaction).filter(Customizable_reaction.chapter == chapter.id ).filter(Customizable_reaction.reaction == generated_question.reaction ).first()
+
+        # Get it's full reaction name
+        page_title = custom_reaction.title
+
+        # recover the generated question
+        instance_full_reaction = generated_question.full_reaction # get the full reaction that has been generated
+        instance_choices = generated_question.choices # get the multiple choices generated
+
+        # Initialize draw SVGRenderer object
+        svg_reanderer = draw.SVGRenderer()
+
+        reaction_svg = svg_reanderer.renderReactionToBuffer(instance_full_reaction, layout=False)
+        reaction_svg = update_svg_size(reaction_svg, '100%', '-1') # update svg size to width to 100%
+
+        # get the correct_choices
+        correct_choices = []
+        for index in range(0,  len(instance_choices)):
             if instance_choices[index][1]:
                 correct_choices.append(str(index))
 
-        session['basename'] = basename
-        print "Started a quiz (products) session for " + basename
+        for instance_choice in instance_choices:
+            instance_choice[1] = False
 
-    # Depending on request parameters, either
-    #   - continue session, or
-    #   - present the answer to problem and a show a button to get a new problem
-    else:
-        basename = session['basename']
-        quiz_type = session['quiz_type']
-        #print "Resuming a quiz (products) session for " + basename
-        reaction = cat.get_reaction_by_basename(basename)
-        full_name = reaction.full_name
+        for answer_index in answer:
+            if answer_index != '':
+                instance_choices[int(answer_index)][1] = True
 
-        if "answer" in request.GET:
+        # check if the answer privided are correct or incorrect
+        # Get the count of how many quiz histories there are.
+        quiz_history_count = DBSession.query(Quiz_history).filter( Quiz_history.user == currentuser.id).filter(Quiz_history.course == course.id).filter(Quiz_history.chapter == chapter.id).count()
+        quiz_history_count = quiz_history_count + 1
 
-            answer = request.GET["answer"].split(",")
+        if set(answer) != set(correct_choices):
+            # record the answer to Quiz_history
+            message = "Incorrect!"
+            result = False
+            DBSession.add(Quiz_history( question_number = quiz_history_count, course = course.id, chapter = chapter.id, user = currentuser.id, score=0, reaction_name = custom_reaction.title, quiz_type=reaction_type, reaction_obj = instance_full_reaction, choice_obj = instance_choices ))
+        else:
+            # record the answer to Quiz_history
+            message = "Correct! You selected what's necessary and nothing else."
+            result = True
+            DBSession.add(Quiz_history( question_number = quiz_history_count, course = course.id,  chapter = chapter.id, user = currentuser.id, score=1, reaction_name = custom_reaction.title, quiz_type=reaction_type, reaction_obj = instance_full_reaction, choice_obj = instance_choices))
 
-            # Invalidate the session
-            state = "tell"
-            session.invalidate()
-            question_svg = ""
+        # remove the generated question from the Generated_question table
+        DBSession.query(Generated_question).filter(Generated_question.id == generated_question.id).delete()
 
-	    for instance_choice in instance_choices:
-		print instance_choice[1]
-		instance_choice[1] = False
+    # elif a current_user has a genereated question then ask the question again.
+    elif generated_question is not None:
 
-	    for answer_index in answer:
-		instance_choices[int(answer_index)][1] = True
+        # update state to ask user the question
+        state = 'ask'
 
-            # Get the count of how many quiz histories there are.
-            quiz_history_count = DBSession.query(Quiz_history).filter( Quiz_history.user == currentuser.id).filter(Quiz_history.course == course.id).filter(Quiz_history.chapter == chapter.id).count()
-            quiz_history_count = quiz_history_count + 1
+        custom_reaction = DBSession.query(Customizable_reaction).filter(Customizable_reaction.chapter == chapter.id ).filter(Customizable_reaction.reaction == generated_question.reaction ).first()
 
-            if set(answer) != set(correct_choices):
-                message = "Wrong!"
-                result = False
-                DBSession.add(Quiz_history( question_number = quiz_history_count, course = course.id, chapter = chapter.id, user = currentuser.id, score=0, reaction_name = mode, quiz_type=quiz_type, reaction_obj = instance_reaction, choice_obj = instance_choices ))
+        # Get it's full reaction name
+        page_title = custom_reaction.title
 
-            else:
-                message = "Correct! You selected what's necessary and nothing else."
-                result = True
-                DBSession.add(Quiz_history( question_number = quiz_history_count, course = course.id,  chapter = chapter.id, user = currentuser.id, score=1, reaction_name = mode, quiz_type=quiz_type, reaction_obj = instance_reaction, choice_obj = instance_choices))
+        instance_full_reaction = generated_question.full_reaction # get the full reaction that has been generated
 
-    owner_courses = []
-    enrolled_courses = []
-    if group["is_teacher"]:
-        owner_courses = Course.owner_courses(request.authenticated_userid)
-    elif group["is_student"]:
-        enrolled_courses = Enrolled.enrolled_courses(request.authenticated_userid)
+        instance_choices = generated_question.choices # get the multiple choices generated
+
+        # Remove the reactant with a question mark to display as the question
+        instance_question = copy.deepcopy(instance_full_reaction)
+        molecule = chem.Molecule()
+        molecule.addAtom(chem.Atom("?", 0, 0, 0, 0, 0))
+        instance_full_reaction.products = [molecule]
+
+        # Initialize draw SVGRenderer object
+        svg_reanderer = draw.SVGRenderer()
+
+        # render instance question to image format svg
+        question_svg = ""
+        question_svg = svg_reanderer.renderReactionToBuffer(instance_full_reaction, layout=False)
+        question_svg = update_svg_size(question_svg, '100%', '-1') # update svg size to width to 100%
+
+        # render the instance_choices to image format svg
+        choices_svg = []
+
+        for index in range(0,  len(instance_choices)):
+            svg = svg_reanderer.renderMoleculeToBuffer(instance_choices[index][0], layout=False)
+            svg = update_svg_size(svg, '100%', '100%') # update svg size to height and width to 100%
+            choices_svg.append([svg, instance_choices[index][1]])    # indicate that these are correct answers
 
     return {
             "layout": logged_layout(),
             "custom_scripts" : custom_scripts,
-            "page_title" : full_name,
+            "page_title" : page_title,
             "basename" : basename,
-            "full_name" : full_name,
             "message" : message,
             "result" : result,
             "is_admin" : group["is_admin"], "is_teacher" : group["is_teacher"], "is_student" : group["is_student"],
@@ -628,10 +654,9 @@ def quiz_product_view(request):
             "owner_courses" : owner_courses,
             "enrolled_courses" : enrolled_courses,
             "logged_in" : request.authenticated_userid,
-            "reaction_svg" : reaction_svg,
+            "choices_svg" : choices_svg,
             "question_svg" : question_svg,
-            "product_svgs" : product_svgs,
-
+            "reaction_svg" : reaction_svg,
         }
 
 
@@ -910,7 +935,7 @@ def quiz_history_view(request):
         }
 
 
-@view_config(route_name='quiz_products', renderer='ltefserver:templates/new/quiz_products.pt', permission='study')
+@view_config(route_name='quiz_products', renderer='ltefserver:templates/new/quiz_products.pt', permission='educate')
 def quiz_products_view(request):
 
         # global variable instance reaction stores the whole reaction question
