f327f1461ed18c330a545b806c2a6e225b988843
DanielKozij <daniel.kozij@gmail.com>
2015-10-02 17:00:15 -0500

    Reactions can be removed and edited.

--- ltefServer/ltefserver/views/reaction.py
+++ ltefServer/ltefserver/views/reaction.py
@@ -72,10 +72,11 @@ def logged_layout():
     renderer = get_renderer("ltefserver:templates/logged_layout.pt")
     layout = renderer.implementation().macros['logged_layout']
     return layout
-		
+
 
 @view_config(route_name='addreaction', renderer='ltefserver:templates/new/addreaction.pt', permission='study')
 def addreaction_view(request):
+
     custom_scripts = []
     group = group_security(request.authenticated_userid)
 
@@ -94,27 +95,96 @@ def addreaction_view(request):
             "is_admin" : group["is_admin"], "is_teacher" : group["is_teacher"], "is_student" : group["is_student"],
 	        "page_title" : "Add New Reaction"			 }
 
+
+@view_config(route_name='reaction_action', match_param='action=edit_reaction', renderer='ltefserver:templates/new/edit_selectable_reaction.pt', permission='educate')
+def edit_reaction_view(request):
+
+    # retrieve all match dictionary
+    course_name = request.matchdict["course"]
+    chapter_name = request.matchdict["chapter"]
+    reaction_name = request.matchdict["reaction"]
+
+    # warning message
+    message = ""
+
+    # retrieve current user
+    currentuser = DBSession.query(User).filter(User.username == request.authenticated_userid).first()
+
+    #add an custom scripts
+    custom_scripts = []
+
+    # retrieve the users permission group and retieve the users courses
+    group = group_security(request.authenticated_userid)
+    owner_courses = []
+    enrolled_courses = []
+    if group["is_teacher"]:
+        owner_courses = Course.owner_courses(request.authenticated_userid)
+    elif group["is_student"]:
+        enrolled_courses = Enrolled.enrolled_courses(request.authenticated_userid)
+
+    # retrieve customizable reaction
+    current_chapter = DBSession.query(Chapter).filter(Course.owner == currentuser.id).filter(Course.name == course_name).filter(Chapter.course == Course.id ).filter(Chapter.title == chapter_name).first()
+    reaction = DBSession.query(Reac).filter(Reac.basename == reaction_name).first()
+    customizable_reaction = DBSession.query(Customizable_reaction).filter(Customizable_reaction.reaction == reaction.id).filter(Customizable_reaction.chapter == current_chapter.id).first()
+
+    # filable raction value, reaction basename, reaction title, and reaction description.
+    reaction_value = customizable_reaction.reaction
+    reaction_title = customizable_reaction.title
+    reaction_description = customizable_reaction.description
+    # All the current information needs to be displayed in the in boxes
+
+    if 'submit.reaction.finish' in request.params:
+
+        customizable_title = request.params['reaction_title']
+        customizable_description = request.params['reaction_description']
+
+        if len(customizable_title) > 0 and len(customizable_description) > 0:
+            DBSession.query(Customizable_reaction).filter(Customizable_reaction.id == customizable_reaction.id).update({ "title":customizable_title,  "description":customizable_description })
+
+            return HTTPFound(location=request.route_url('home') + 'class/' + course_name)
+        else:
+            message = "Both inputs are required"
+
+    return {"layout": logged_layout(),
+	        "custom_scripts" : custom_scripts,
+            "message" : message,
+            "owner_courses" : owner_courses,
+            "enrolled_courses" : enrolled_courses,
+            "logged_in" : request.authenticated_userid,
+            "is_admin" : group["is_admin"], "is_teacher" : group["is_teacher"], "is_student" : group["is_student"],
+	        "page_title" : "Edit Reaction",
+            "course_name" : course_name,
+            "chapter_name" : chapter_name ,
+            "reaction_name" : reaction_name,
+            "reaction_title" : reaction_title,
+            "reaction_description" : reaction_description,
+            }
+
+
+
+
+
 @view_config(route_name='reaction_action', match_param='action=remove_reaction', permission='educate')
 def remove_reaction_view(request):
 
-
-    course = request.matchdict["course"]
+    # retrieve all match dictionary
+    course_name = request.matchdict["course"]
     chapter_name = request.matchdict["chapter"]
-    custom_scripts = []
-    message = ""
-    chapter_title = ""
-    course_description = ""
+    reaction_name = request.matchdict["reaction"]
+
+    # retrieve current user
     currentuser = DBSession.query(User).filter(User.username == request.authenticated_userid).first()
-    group = group_security(request.authenticated_userid)
 
-    current_chapter = DBSession.query(Chapter).filter(Course.owner == currentuser.id).filter(Chapter.course == Course.id ).filter(Chapter.title == chapter_name).first()
+    # retrieve customizable reaction
+    current_chapter = DBSession.query(Chapter).filter(Course.owner == currentuser.id).filter(Course.name == course_name).filter(Chapter.course == Course.id ).filter(Chapter.title == chapter_name).first()
+    reaction = DBSession.query(Reac).filter(Reac.basename == reaction_name).first()
+    customizable_reaction = DBSession.query(Customizable_reaction).filter(Customizable_reaction.reaction == reaction.id).filter(Customizable_reaction.chapter == current_chapter.id).first()
 
-    DBSession.query(Chapter).filter(Chapter.id == current_chapter.id).delete()
-    DBSession.query(Customizable_reaction).filter(Customizable_reaction.chapter == current_chapter.id).delete()
+    # remove customizable reaction
+    DBSession.query(Customizable_reaction).filter(Customizable_reaction.id == customizable_reaction.id).delete()
 
-    return HTTPFound(location=request.route_url('home') + 'class/' + course) 
-
-
+    # return user back to the course home page
+    return HTTPFound(location=request.route_url('home') + 'class/' + course_name)
 
 
 @view_config(route_name='editlist', renderer='ltefserver:templates/new/editlist.pt', permission='educate')
